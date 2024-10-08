b95233654281af953d75a0a557a07e5dd0bd5c10
Aydar Work <ShanturovaV@venteko.local>
2024-10-08 17:44:55 +0500

    lambda 08/10/24

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,83 +1,128 @@
-fun main(){
-    val noteService = ServiceCRUD<Note>()
-    val commentService = ServiceCRUD<Comment>()
-    val addedNote = noteService.create(Note())
-    val addedComment = commentService.create(Comment())
-    val addedNote2 = noteService.create(Note())
-    val addedComment2 = commentService.create(Comment())
+fun main() {
+    val messageService = MessageService()
+    val chatService = ChatService()
 }
 
-data class Note(
-    var id: Int = 0,
-    var text: String = "0",
-    val comment: Comment = Comment(),
-    var isDeleted: Boolean = false
+data class Message(
+    var id: Int = 0, // id сообщения
+    val idChat: Int = 0, // название собеседника
+    var isDeleted: Boolean = false, //  удаленность сообщения
+    var text: String = "0",//  само сообщение
+    var readMessage: Boolean = false, // прочитано сообщение?
+    val sent: Boolean = true // отправленное или полученное сообщение
 )
 
-
-data class Comment(
-    var id: Int = 0,
-    val noteId: Int = 0,
-    var text: String = "0",
-    var isDeleted: Boolean = false
-)
+data class Chat(
+    var id: Int = 0, // id сообщения
+    val title: String = "Name", // название собеседника
+    var isDeleted: Boolean = false,//  удаленность чата
+    val message: Message = Message(),
+    )
 
 
 interface Service<T> {
     fun create(item: T): T
     fun read(id: Int): T?
-    fun update(item: T): T?
     fun delete(id: Int): Boolean
+    fun show()
+
 }
 
-
-class ServiceCRUD<T>(private val items: MutableList<T> = mutableListOf()) : Service<T> {
+class MessageService(private val messages: MutableList<Message> = mutableListOf()) : Service<Message> {
     private var i = 1
 
-    override fun create(item: T): T {
-        if (item is Note) {
-            item.id = i++
-        } else if (item is Comment) {
-            item.id = i++
-        }
-        items.add(item)
-        return item
+    override fun create(message: Message): Message {
+        message.id = i++
+        messages.add(message)
+        val chatService = ChatService()
+        chatService.create(Chat(message = message))
+
+        return message
     }
 
-    override fun read(id: Int): T? {
-        return items.find {
-            if (it is Note) {
-                it.id == id
-            } else if (it is Comment) {
-                it.id == id
-            } else {
-                false
-            }
-        }
+    override fun read(id: Int): Message? { //+
+        return messages.find { it.id == id }
     }
 
-    override fun update(item: T): T? {
-        val index =
-            items.indexOfFirst { (it as? Note)?.id == (item as? Note)?.id || (it as? Comment)?.id == (item as? Comment)?.id }
+    fun update(message: Message): Message? { //+
+        val index = messages.indexOfFirst { it.id == message.id }
         if (index != -1) {
-            items[index] = item
-            return item
+            messages[index] = message
+            return message
         }
         return null
     }
 
+    override fun delete(id: Int): Boolean {
+        val message = read(id)
+        return if (message != null) {
+            message.isDeleted = true
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun show() {
+        for (message in messages) {
+            println("ID: ${message.id}, Текст: ${message.text}")
+        }
+    }
+
+    fun readStatusChange(id: Int): Boolean {
+        val message = read(id)
+        return if (message != null) {
+            message.readMessage = true
+            true
+        } else {
+            false
+        }
+    }
+}
+
+
+class ChatService(private val chats: MutableList<Chat> = mutableListOf()) : Service<Chat> {
+    private var i = 1
+
+    override fun create(chat: Chat): Chat { //когда  отправляется сообщение
+        chat.id = i++
+        chats.add(chat)
+        return chat
+    }
+
+    override fun read(id: Int): Chat? {
+        return chats.find { it.id == id }
+    }
+
+    fun getUnreadChatsCount(): Int? {
+        var count = 0
+        return count
+    }
 
     override fun delete(id: Int): Boolean {
-        val item = read(id)
-        if (item != null) {
-            if (item is Note) {
-                item.isDeleted = true
-                return true
-            } else if (item is Comment) {
-                item.isDeleted = true
-                return true
-            }
+        val chat = read(id)
+        return if (chat != null) {
+            chat.isDeleted = true
+            true
+        } else {
+            false
         }
-        return false
+    }
+
+    override fun show() {
+        for (chat in chats) {
+            println("ID: ${chat.id}, Текст: ${chat.title}")
+        }
+    }
+
+    fun getLastChatsCount(): Int {
+        var count = 0
+        return count
+    }
+
+    fun showId() {
+        for (chat in chats) {
+            println("ID: ${chat.id}, Текст: ${chat.title}")
+        }
     }
 }
